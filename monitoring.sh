c73d29861fe210be7615c1fc30e812a2159f1be9
lulm <lionelulm67@gmail.com>
2024-03-20 11:56:33 +0100

    modified monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -5,6 +5,8 @@ cpuv=$(cat /proc/cpuinfo | grep processor | wc -l)
 ramu=$(free -m | grep Mem | awk '$1 == "Mem:" {print $2, $3}')
 ramp=$(free | awk '$1 == "Mem:" {printf("$.2f"), $3/$2*100}')
 cpuu=$(grep 'cpu' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}')
+rebo=$(who -b | awk '{print $3, $4}')
+
 	echo ${archi}
 	echo ${cpup}
 	echo ${cpuv}
